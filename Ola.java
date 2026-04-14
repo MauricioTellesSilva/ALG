030f6f6d1847980f4a5e400fc39a62ee1c202367
MauricioTellesSilva <mauriciotellessilva@gmail.com>
2026-04-14 19:12:36 -0300

    create

---
+++ Ola.java
@@ -0,0 +1,22 @@
+void main(){
+    boolean i;
+    i=true;
+    while (i=true){
+    IO.println("H");
+    IO.println("He");   
+    IO.println("Hel");
+    IO.println("Hell");
+    IO.println("Hello");
+    IO.println("Hello w");
+    IO.println("Hello wo");
+    IO.println("Hello worl");
+    IO.println("Hello world");
+    IO.println("Hello worl");
+    IO.println("Hello wo");
+    IO.println("Hello w");
+    IO.println("Hello");
+    IO.println("Hell");
+    IO.println("Hel");
+    IO.println("He");
+    }
+}
