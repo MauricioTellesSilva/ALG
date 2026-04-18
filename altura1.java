9d009505f19ba4033bfd6d9855256dd07884a34a
MauricioTellesSilva <mauriciotellessilva@gmail.com>
2026-04-17 21:14:27 -0300

    Troca p scanner

--- altura1.java
+++ altura1.java
@@ -1,18 +1,15 @@
+
+import java.util.Scanner;
+
 void main() {
-    String entrada;
-    Double altura;
+    Scanner sc = new Scanner(System.in);
+    double altura =0;
     IO.println("Digite sua altura em pés + polegadas:");
-     entrada =IO.readln("Digite a altura em pés:");
-     Double pes = Double.parseDouble(entrada);
-     altura = ((pes*12)*2.54);
-     entrada =IO.readln("Digite a altura em polegadas:");
-     Double pol = Double.parseDouble(entrada);
-     altura += (pol*2.54);
-     altura /= 100;
-     IO.println("Sua altura: "+altura+"m");
-
-
-
-
-    
+    Double pes = sc.nextDouble();
+    Double pol = sc.nextDouble();
+    altura += ((pes*12)*2.54);
+    altura += (pol*2.54);
+    altura /= 100;
+    IO.println("Sua altura: "+altura+"m");
+    sc.close();  
 }
