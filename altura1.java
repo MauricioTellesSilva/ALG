abe4f32d7d7adcaf82db501f115e635b1bb66475
MauricioTellesSilva <mauriciotellessilva@gmail.com>
2026-04-17 21:03:49 -0300

    Logica altura1.java

--- altura1.java
+++ altura1.java
@@ -1,5 +1,16 @@
 void main() {
-    
+    String entrada;
+    Double altura;
+    IO.println("Digite sua altura em pés + polegadas:");
+     entrada =IO.readln("Digite a altura em pés:");
+     Double pes = Double.parseDouble(entrada);
+     altura = ((pes*12)*2.54);
+     entrada =IO.readln("Digite a altura em polegadas:");
+     Double pol = Double.parseDouble(entrada);
+     altura += (pol*2.54);
+     altura /= 100;
+     IO.println("Sua altura: "+altura+"m");
+
 
 
 
