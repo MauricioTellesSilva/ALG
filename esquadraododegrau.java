80d1b70d98727ab4c0935aa1066b4ab974a6ad14
MauricioTellesSilva <mauriciotellessilva@gmail.com>
2026-06-02 18:56:52 -0300

    loop while

---
+++ esquadraododegrau.java
@@ -0,0 +1,18 @@
+void main() {
+    String buffer = IO.readln("Entre com um inteiro não-negativo: ");
+    int n = Integer.parseInt(buffer);
+    if (ehDregrau(n)) {
+        IO.println(n + " é um agente do esquadrão do Degrau.");
+    } else {
+        IO.println(n + " não é um agente do esquadrão do Degrau.");
+    }
+
+}
+
+boolean ehDregrau(int n) {
+    int digito, soma = 0;
+    while (true){
+        
+    }
+
+}
