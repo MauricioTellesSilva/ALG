cbe26613769dbfa38f2a0a2b0dab44431338cede
MauricioTellesSilva <Mauriciotellessilva@gmail.com>
2026-06-20 21:32:29 -0300

    code Rodadosventos

--- Rodadosventos.java
+++ Rodadosventos.java
@@ -1,3 +1,39 @@
 void main(){
-    
+    String buffer= IO.readln("Digite o angulo: ");
+    int angulo=Integer.parseInt(buffer);
+    int voltas=angulo/360;
+    angulo=angulo-(voltas*360);
+    int dir=angulo/45;
+    String Ndir= "";
+    IO.println(angulo);
+    IO.println(dir);
+    switch(dir) {
+        case 0:
+            Ndir="N";
+        break;
+        case 1:
+            Ndir="NE";
+        break;
+        case 2:
+            Ndir="E";
+        break;
+        case 3:
+            Ndir="SE";
+        break;
+        case 4:
+            Ndir="S";
+        break;
+        case 5:
+            Ndir="SW";
+        break;
+        case 6:
+            Ndir="W";
+        break;
+        case 7:
+            Ndir="NW";
+        break;
+        default:
+            break;
+    }
+    IO.println(Ndir);
 }
