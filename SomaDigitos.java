fe15a324ba03570c24c3d4c411c1b0758d3ad636
MauricioTellesSilva <Mauriciotellessilva@gmail.com>
2026-06-20 22:01:09 -0300

    code SomaDigitos.java

---
+++ SomaDigitos.java
@@ -0,0 +1,17 @@
+void main() {
+    String buffer=IO.readln("Digite um numero inteiro nao negativo:");
+    int n=Integer.parseInt(buffer);
+    int soma=SomaDigitos(n);
+    IO.println(soma);
+}
+int SomaDigitos(int n){
+    int resto=0;
+    int s=0;
+    int soma=0;
+    while (n>0){
+        s=n%10;
+        n/=10;
+        soma=soma+s;
+    }
+    return soma;
+}
