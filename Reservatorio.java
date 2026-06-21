cc99af5ae0f19f69fa4cb957a6ebc76f2db882dd
MauricioTellesSilva <Mauriciotellessilva@gmail.com>
2026-06-21 19:17:04 -0300

    code Reservatorio

--- Reservatorio.java
+++ Reservatorio.java
@@ -1,13 +1,19 @@
 void main(){
     String buffer="";
     int capamax=400,capasem=0, capadia=0;
-    for(int i =0;i>7;i++){
-        buffer=IO.readln("Entre com a quantidade de chuva no dia "+i+" em litros: ");
+    boolean transbordou=false;
+    for(int i =0;i<7;i++){
+        buffer=IO.readln("Entre com a quantidade de chuva no dia "+(i+1)+" em litros: ");
         capadia=Integer.parseInt(buffer);
         capasem=capadia+capasem;
         if (capasem>=capamax){
-            IO.println("O reservatorio vazou no dia "+i);
+            transbordou=true;
+            IO.println("O reservatorio vazou no dia "+(i+1));
             break;
         }
-    }    
+    }
+    if (!transbordou){
+        IO.println("O reservatorio nao transbordou. Espaco livre: "+(capamax-capasem));
+    }
+        
 }
