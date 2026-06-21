ff240d065537dd9c8784c825a10d4d9b21b076b0
MauricioTellesSilva <Mauriciotellessilva@gmail.com>
2026-06-21 15:22:05 -0300

    logica peixes

--- Peixes.java
+++ Peixes.java
@@ -1,3 +1,24 @@
 void main () {
-    String 
+    String buffer =IO.readln("Digite o Ph minimo: ");
+    Double Phmn=Double.parseDouble(buffer);
+    buffer =IO.readln("Digite o Ph maximo: ");
+    Double Phmax=Double.parseDouble(buffer);
+    buffer =IO.readln("Digite o numero de medicoes: ");
+    int md=Integer.parseInt(buffer);
+    int i=1;
+    int frmd=0;
+    int interacao=0;
+    while (md>i){
+        buffer=IO.readln("Digite a medicao "+i+" :");
+        interacao=Integer.parseInt(buffer);
+        if (interacao>Phmax){
+
+        } else if (interacao<Phmn){
+
+        } else {
+            
+        }
+    }
+
+
 }
